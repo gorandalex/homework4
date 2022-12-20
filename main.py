40839c604750cb94f1f4e96dff5f4a9db3f7ffd2
gorandalex <andrii.gorobets@gmail.com>
2022-12-20 13:05:08 +0200

    add_docker_compose

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import mimetypes
 import pathlib
-
 import udp_client
 import udp_server
 from threading import Thread
 import urllib.parse
 
 HOST = 'localhost'
-PORT = 3000
+PORT = 5000
 
 
 class HttpHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        print("Get HTTP request")
         pr_url = urllib.parse.urlparse(self.path)
         match pr_url.path:
             case '/':
@@ -27,6 +27,7 @@ class HttpHandler(BaseHTTPRequestHandler):
                     self.send_html_file('error.html', 404)
 
     def do_POST(self):
+        print("Post HTTP request")
         data_input: bytes = self.rfile.read(int(self.headers['Content-Length']))
         data_decode: str = urllib.parse.unquote_plus(data_input.decode())
         data: dict = urllib.parse.parse_qs(data_decode)
@@ -59,21 +60,38 @@ class HttpHandler(BaseHTTPRequestHandler):
 
 
 def run(server_class=HTTPServer, handler_class=HttpHandler):
+    print("run")
     path_data = pathlib.Path.joinpath(pathlib.Path().absolute(), 'storage')
+    print(path_data.absolute())
     if not path_data.exists():
+        print('Creating directory')
         pathlib.Path.mkdir(path_data)
-        path_data = pathlib.Path.joinpath(path_data, 'data.json')
+        print('Creating file')
+    path_data = pathlib.Path.joinpath(path_data, 'data.json')
+    print(path_data.absolute())
+    if not path_data.exists():
         path_data.touch(exist_ok=True)
         path_data.write_text('{}')
-    server_address = ('', 5000)
+        print('File is saved')
+    if not path_data.exists():
+        print('File is not created')
+    else:
+        print('File is created')
+    server_address = ('localhost', 3000)
     http = server_class(server_address, handler_class)
     try:
+#        http.serve_forever()
         server = Thread(target=udp_server.udp_server, args=(HOST, PORT))
+        http_server = Thread(target=http.serve_forever)
+        print(f'{http =}')
+#        http_server.daemon = True
         server.start()
-        http.serve_forever()
+        http_server.start()
+        print('Server is running')
     except KeyboardInterrupt:
         http.server_close()
 
 
 if __name__ == '__main__':
+    print('main')
     run()
